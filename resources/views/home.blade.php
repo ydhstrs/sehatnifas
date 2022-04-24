64d8ce5401e6b263b879fa620c2eb93a078c473a
ydhstrs <yudhatriya07@gmail.com>
2022-04-25 00:59:59 +0700

    Update Content Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -130,7 +130,7 @@
                 tahap ini untuk memulihkan diri dan menjadi sehat dalam kondisi luar biasa.
                 Lamanya masa nifas yng jauh bagi setiap ibu adalah unik, bergantung pada
                 beratnya keselitan yang dialami ibu. Kesehatan yang luar biasa dapat berlangsung
-                cukup lama, berbuln-bulan atau bahkan bertahun.
+                cukup lama, berbuln-bulan atau bahkan bertahun. 
             </p>
         </div>
         <div class="col-md-5">
@@ -226,7 +226,168 @@
                 yang khusus, bantuan dan pengawasan demi pulihnya kesehatan.salah satu cara untuk mempercepat involusi
                 uterus yaitu denganmelakukan senam nifas yang bertujuan merangsang otot-otot rahim agar berfungsi secara
                 optimal sehingga diharapkan tidak terjadi perdarahan post partum dan mengambilkan uterus pada posisi
-                seperti semula.</p>
+                seperti semula. Latihan atau exercise tidak hanya bermanfaat bagi ibu nifas untuk merangsang otot rahim, latihan peregangan otot juga dianjurkan pada remaja wanita yang mengalami dismenorhea.
+            </p>
+            <p class="lead">
+                Abdominal stretching Exercisemerupakan suatu latihan peregangan otot terutama pada perut yang dilakukan selama 10 menit. Latihan ini dirangcang untuk meningkatkan kekuatan oyot, daya tahan dan fleksibilitas otot, sehingga diharapkan dapat menurunkan nyeri haid (disminore) pada wanita (mindarsih, 2020). Senam nifas jika dilakukan setiap hari bermanfaat untuk mengencangkan otot perut dan mengurangi keluhan sakit punggung ibu nifas.
+            </p>
+            <p class="h5">
+                1. Tujuan Senam Nifas
+            </p>
+            <p class="lead">
+                a.	 Meningkatkan sirkulasi darah, sehingga mencegah pembentukan bekuan darah ( thrombosis)<br>
+                b.	 Mengencangkan dan meningkatkan tonus perut, hubungan intim, otot sekitar vagina dan otot dasar panggul serta otot kaki.<br>
+                c.	 Membantu ibu sembuh dari ketergantungan sakit menjadi sehat.
+
+            </p>
+            <p class="h5">
+                2. Kontrak indikasi senam nifas, ibu yang mengalami komplikasi nifas tidak di  perbolehkan melakukan senam. 
+            </p>
+            <p class="h5">
+                3. Waktu senam nifas ini dilakukan saat ibu benar-benar pulih dan tidak ada koplikasi obstetric atau penyakit masa nifas ( seperti tekanan darah tinggi, pasca kejang , dan demam)
+            </p>
+            <p class="h4">
+                3.	Perawatan Payudara
+            </p>
+            <p class="lead">
+                Pembengkakan payudara adalah akibat bendungan yang terjadi karena penyempitan duktus lakitiferus atau kelenjar tidak dikosongkan secara sempurna sehingga panyudara terasa lebih penuh, panas, keras, dan nyeri, disertai dengan kenaikan suhu tubuh.Menurut ( nasution, dkk,.2018) perawatan payudara harus dilalukan segera setelah proses persalinan bayi (1-2 hari), dan ibu harus melakukannya secara teratur. Dengan memberikan rangsangan pada otot-otot payudara akan membantu merangsang hormone prolaktin untuk membantu produksi ASI. Untuk mempercepat proses pengeluaran ASI diperlukan proses perawatan panyudara pada ibu nifas. beberapa perawatan yang dilakukan sering termasuk pijak oksitosin, akupresur dan akupunkur. Namun proses perawatan payudara tentunya tidak terlepas dari perkembangan teknologi di bidang kesehatan sehingga akan muncul beberapa inovasi terbaru alam perkembangan perawatan payudara. sehingga setiap ibu nifas dapat melakukan perawatan payudara secara mandiri (Nasution,dkk.,2018).Perawatan payudara berkaitan dengan proses laktasi dan menyusui. Laktasi dimulai pada semua wanita dengan perubahan hormone saat ibu melahirkan. Keputusan ibu apakah memilih menyusui atau tidak sangat dipengaruhi oleh dukungan keluarga dan kebiasanya serta budaya masyarakat sekitarnya. Wanita yang menyusui berespon terhadap bayi yang disusuinya dan mentimul alveoli untuk memproduksi ASI. Pengkajian panyudara pada periode awal pasca iritasijaringan payudara, adanya kolostrum. Apakah payudara terisi air susu dan adanya sumbatan duktus, kongesti dan tanda-tanda mastitis potensial (Nasution, S.S.2018).
+            </p>
+            <p class="lead">
+                Teknik menyusui yang benar adalah cara memberikan ASI kepada bayi dengan perletakan dan posisi ibu dan bayi dengan benar. Untuk mencapai keberhasilan menyusui diperlukan pengetahuan mengenai teknik-teknik menyususi yang benar. proses menyususi yang perlu dilakukan dan ditaati ibu menyusui pasca melahirkan, paling sedikit enam bulan. Ibu menyusui perlu manajemen diri yang kuat dalam sadar diri dan determinasi diri. Pengetahuan dan sikap ibu menyusui tentang manajemen laktasi sangat mempengaruhi ibu menyusui dalam pemberian ASI,dimana laktasi merupakan keseluruhan proses menyusui mulai dari ASI diproduksi sampai proses bayi menghisap dan menelan ASI(keni, dkk.,2020)
+            </p>
+            <p class="lead">
+                Perawatan payudara adalah suatu tindakan untuk merawat payudara terutama pada masa nifas (masa menyusui) untuk melancarkan pengeluaran Asi. Perawatan payudara pasca persalianan adalah lanjutan perawatan payudara semasa hamil. Manfaatnya yaitu Melancarakan pengeluaran Asi, merangsang produksi Asi, mencegah bendunganpayudara dan mengurangi resiko luka pada saat menyusui.
+            </p>
+            <p class="h5">
+                1. Cuci tangan sebelum mases. Lalu tuangkan minyak kedua belah telapak tangan secukupnya. Pengurutan dimulai dari ujung jari, caranya: <br>
+                2. Sokong payudara kiri dengan tangan kiri lakukan gerakan kecil dengan 2 atau 3 jari tangan mulai dari pangkal payudara dan berakhir dengan gerakan spiral dengan daerah putting susu. <br>
+                3. Selanjutnya dilakukan buatlah gerakan memutar sambil menekan dari pangkal payudara dan berakhir pada putting susu diseluruh dibagian payudara. Lakukan gerakan seperti ini pada payudara sebelah kanan <br>
+                4. Gerakan selanjutnya letakan kedua telapak tangan diantara kedua payudara. Urutlah dari tengah ke atas sambil mengangkat kedua payudara dan lepaskan keduanya perlahan. Lakukan gerakan ini ±30 kali. <br>
+                5. Cobalah posisi tangan paralel. Sanggah payudara dengan satu tangan sedangkan tangan lain mengurut payudara dari sisi kelingking dari arah pangkal payudara kea rah putting usus. Lakukan gerakan ini selama 30 kali. <br>
+
+            </p>
+            <p class="lead">
+                Apabila payudara bengkak atau lancet maka lakukan perawatan payudara dapat dilakuakn dengan cara :
+            </p>
+            <p class="h5">
+                1. Menjaga payudara tetap bersih dan kering, terutama puting susu <br>
+                2. Menggunakan BH yang menyokong payudara <br>
+                3. Mengistirahatkan payudara apabila lecet sangat berat selama 24 jam <br>
+                4. Meminum paracetamol setiap 4-5 jam untuk menghilangkan nyeri <br>
+                5. Apabila payudara bengkak akibat bendungan Asi, lakukan pengempresan payudara dengan menggunakan kain basah dan hangat selama 5 menit, mengurut payudara dari pangkal menuju putting mrnggunakan sisir untuk mengurut payudara dengan arah Z menuju putting, keluarkan susu sebagian dari bagian depan payudara sehingga putting susu menjadi lunak bayi di susuisetiap 2-3 jam dan apabila tidak dapat menghisap seluruh Asi sisanya dikeluarkan dengan tangan baru meletakkan kain dingin pada payudara setelah menyusi, payudara dikeringkan (mardiatun, 2013).<br>
+            </p>
+            <p class="h4">
+                4.	Meningkatkan Kebutuhan Nutrisi
+            </p>
+            <p class="lead">
+                Kebutuhan nutrisi seharusnya dimulai sejak mas kehamilan untuk menjaga kondisi kesehatan janin dan juga sang ibu. Nutrisi adekuat pada masa kehamilan diperlukan untuk dapat mengurangi resiko komplikasi pada ibu, membantu dalam pertumbuhan jaringan dan meningkatkan berat badan janin secaraoptimal sehingga bayi dapat lahir secara sehat. Kebutuhan nutrisi sangat penting selama proses pemulihan pasca melahirkan, kebutuhan nutrisi yang mencukupi dapar memenuhi asupan ASI dan juga mempercepat proses penyembuhan pada ibu (nadrah, 2020).
+            </p>
+            <p class="h4">
+                5.	Eliminasi
+            </p>
+            <p class="h5">
+                1.	Buang Air kecil (BAK)
+            </p>
+            <p class="lead">
+                ibu diminta untuk buang air kecil 6 jam post partum. Jika dalam 8 jam post partum belum dapat berkemih atau sekali berkemih belum melalui 100cc, maka dilakukan kateterisasi. Akan tetapi, jika ternaya kandung kemih penuh, tidak perlu menunggu 8 jam untuk ketetrriasi
+            </p>
+            <p class="h5">
+                2.	Buang Air Besar (BAB)
+            </p>
+            <p class="lead">
+                Ibu post partum diharapkan dapat BAB setelah hari kedua post partum. Jika hari ketiga belum juga BAB, maka perlu diberi obat pencahar per oral atau per rectal. Jika setelah pemberian obat pencahar masih belum bisa BAB , maka dilakukan klisma (huknah)
+            </p>
+            <p class="h4">
+                6. Istirahat
+            </p>
+            <p class="lead">
+                Ibu harus istirahat tidur telentang selama 8 jam psca persalinan.kemungkinan boleh miring kekanan kekiri untuk mencegah terjadinya thrombosis dan trombo emboli. Pada hari kedua diperbolehkan duduk, hari ketiga berjalan-jalan dan hari keempat atau kelima sudah diperbolehkan pulang. Keuntungan dari mobilisasi dini adalah melancarkan pengeluaran lochia, mengurangi infeksi purperium, mempercepat involusi alat kandungan, melancarkan fungsi alat gastrointestinal dan alat perkemihan, meningkatkan kelancaran peredaran darah sehingga mempercepat fungsi ASI dan pengeluaran sisa metabolisme (Nasution, S.S, 2018).
+            </p>
+
+        </div>
+        <hr class="featurette-divider">
+
+    <div class="row featurette">
+                <div class="col-md-7 order-md-2">
+
+            <h2>Tanda Bahaya Nifas</h2>
+            <p class="lead">Penjelasan pada wanita atau ibu nifas mengenai tanda-tanda bahaya masa nifas sangat penting dan perlu, oleh karena masih banyak ibu atau wanita yang sedang hamil atau pada masa nifas belum mengetahi tentang tanda-tanda bahaya masa nifas, baik yang diakibatkan masuknya kumat ke dalam alat kandunganseperti kuman oksogen ( kuman dating dari luar), autogen (kuman masuk dari tempat lain dalam tubuh dan endogen dari jalan lahir sendiri) (siallagan, dkk.2020).Tanda bahaya masa nifas adalah keadan abnormal yang mengindikasikan adanya bahaya/komplikasi yang akan terjadi pada ibu nifas, dimana jika tidak ditangani dan dilaporkan akan sangat berbahaya bagi kesehatan ibu nifas.
+            </p>
+            <p class="h4">
+                1.	Pendarahan postpartum
+            </p>
+            <p class="lead">
+            Pendarahan postpartum adalah pendarahan lebih dari 500cc yang terjadi setelah bayi lahir pervaginam atau lebih dari 1.000 ml setelah persalina abdominal. Kondisi dalam persalinan menyebabkan kesulitan untuk menentukan jumlah pendarahan disebutkan sebagai perdarahan yang lebih dari normal yang telah menyebabkan perubahan tanda vital, antara lain pasien mengeluh lemah, berkeringat dingin, mengigil, hipernea, tekanan darah sistolik < 90 mmHg, denyut nadi >100/menit, kadar Hb >8 g/dl (hikmah dan yani, 2015).
+            <br>
+            Perdarahan post partum dibedakan menjadi dua yaitu:
+            <br>
+            a. Pendarahan postpartum primer yakni pendarahan yang terjadi pada 24 jam pertama setelah anak lahir sebanyak 500-600 ml, yang disebabkan biasanya terjadi pada hari ke 5 sampai hari ke 15 postpartum, yang disebabkan oleh robekan jalan lahir dan sisa plasenta atau membrane.
+            <br>
+            b. Pendarahan postpartum sekunder yakni perdarahan yang terjadi setelah 24 jam pertama hingga selesai masa nifas. Pendarahan sekunder biasanya beberapa perkembangan definisi perdarahan
+            </p>
+            <p class="h4">
+                2.  Lochea Berbau Busuk
+            </p>
+            <p class="lead">
+                Lochea adalah sekret yang berasa dari dalam rahim terutama luka bekas implantasi plasenta yang keluar dari vagina. Lochea merupakan pembersihan uterus setelah melahirkan yang mikroakopik terdiri dari eritrosit, jaringan desidua, sel-sel epitel dan bakteri yang dikeluarkan pada awal masa nifas.Lochea ini dibagi berdasarkan warna dan kandungannya:
+            </p>
+            <p class="h5">
+                 a. Lochea rubra 
+            </p>
+            <p class="lead">
+                Keluar pada hari pertama sampai hari ketiga post partum. Lochea ini berwarna merah berisi eritrosit, lekosit, sel-sel desidua, vernik kaseosa, rambut lanugo, sis mekonium dan sisa-sisa selaput ketuban
+            </p>
+            <p class="h5">
+                b. Lochea serosa
+            </p>
+            <p class="lead">
+                Lochea ini keluar keempat sampai kesepuluh post partum dengan warna kuning kecoklatan. Mengandung sel darah, serum, leukosit dan sisa-sisa jaringan dan sejumlah mikroorganisme.
+            </p>
+            <p class="h5">
+                c. Lochea alba 
+            </p>
+            <p class="lead">
+                Lochea ini keluar mulai pada hari kesepuluh sampai minggu ke 2-6 post partum. Berwarna putih kekuningan, tidak mengandung darah, berisi sel leukosit, sel-sel epitel dan lendir serviks.Jika pengeluaran lochea berkepanjangan, pengeluaran lochea tertahan, lochea yang spurulenta(nanah), rasa nyeri yang berlebihan, terdapat sisa plasenta merupakan indikasi perdarahan dan infeksi intra uterin
+            </p>
+            <p class="h4">
+                3.	Sub Involusi Uterus 
+            </p>
+            <p class="lead">
+                Sub involusi uterus terjadi karena adanya sisa plasenta dan infeksi. Sub involusi uterus menyebabkan kontrasi uterus menurun sehingga pembuluh darah yang lebar tidak terjadi terus menerus. Pada pemeriksaan bimanual ditemukan uterus lebih besar dan lebih lembek dari seharusnya. Fundus masih tinggi, lochea banyak dan berbau, dan tidak jarang terdapat pula pendarahan (fairus, 2017).
+            </p>
+            <p class="h4">
+                4.	Infeksi pada masa nifas
+            </p>
+            <p class="lead">
+                Infeksi masa nifas masih berperan sebagai penyebab utama kematian ibu terutama di Negara berkembang seperti di Indonesia, masalah itu terjadi akibat pelayanan perawatan yang masih jauh dari kata sempurna. Infeksi pada masa nifas adalah masuknya bakteri atau mikroorganisme pada traktus genitalia setelah persalinan selesai, suhu naik hingga 380 C atau lebih selama 2 haridalam 10 hari pertama selesai melahirkan, dengan pengecualian 24 jam pertama (hasibuan, 2021).
+                <br>
+                Bakteri Stabylococcus aureus menyebabkan salah satu dari penyakit infeksi yang telah dilaporkan mengalami peningkatan di seluruh dunia. Stabylococcus aureus sering menimbulkan infeksi nosokomial pada bayi, pasien luka bakar, dan pasien bedah di rumah sakit. Selain itu stabylococcus aureus juga dapat menyebabkan infeksi luka jahitan di daerah perineum sebanyak 50% dan infeksi luka bekas oprasi Caesar (wijayanti, 2018).
+                <br>
+                Faktor pemicu yang menyebabkan terjadinya masa nifas adalah:
+            </p>
+            <p class="h5">
+                1. Alat-alat yang digunakan tidak sepenuhnya steril <br>
+                2. Terjadinya kontraminasi bakteri dari hidung atau tenggorokan petugas kesehatan yang menderitainfeksi saluran pernafasan <br>
+                3. Hubungan seksual di akhir kehamilan dapat menyebabakan pecahnya ketuban sehingga berisiko masuknya bakteri penyebab infeksi <br>
+            </p>
+            <p class="lead">
+                Tanda dan gejala infeksi secara umum menurut kutipan ( hasibuan, 2021).
+            </p>
+            <p class="h5">
+                1. Rasa sakit atau tidak nyaman, dengan atau tanpa pembengkakan di area 
+                abdominal bawah usai beberapa hari melahirkan <br>
+                2. Rasa sakit yang tak kunjung di daerah parineal <br>
+                3. Bengkak ditempat tertentu dan kemerahan, panas, dan keluar darah di tempat insisi ceaser <br>
+                4. Sakit kepala, pusing dan lemas yang berlebihan serta pengelihatan kabur.Sakit kepala adalah suatu kondisi sakit kepala (terkadang sakit dibelakang leher atau punggung atas). Sakit kepala yang parah dapat menyebabkan penglihatan kabur atau bayangan, menyebabakan edema otak dan menyebabakan resistensi otak yang mempengaruhi sistem saraf pusat, mengakibatkan kelainan serebral(sakit kepala, kejang) dan gangguan penglihatan . <br>
+            </p>
+            <p class="h4">
+                5. Payudara bengkak
+            </p>
+            <p class="lead">
+                Pembengkakam payudara adalah akibat bendungan yang terjadi karena penyempitan duktus lakitiferus atau kelenjar tidak dikosongkan secara sempurna sehingga panyudara terasa lebih penuh, panas, keras, dan nyeri, disertai dengan kenaikan suhu tubuh.
+            </p>
+            
+            
         </div>
         <div class="col-md-5">
             <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
